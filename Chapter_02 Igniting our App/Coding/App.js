4bee35b569944c80dc2837773b80bea6fe0b5434
md musharruf <mdmusharruf13@gmail.com>
2023-12-20 19:19:05 +0530

    chapter 2 js file

---
+++ Chapter_02 Igniting our App/Coding/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement("div",
+    { id: 'parent' },
+    React.createElement("div", { id: 'child' }, [
+        React.createElement("h1", {}, "i'm an h1 tag"),
+        React.createElement("h1", {}, "i'm an h1 tag"),
+    ])
+);
+
+
+root.render(heading);
