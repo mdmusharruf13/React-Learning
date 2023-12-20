5daa9ad087cd3497251d448fd6a1a0f6ae3053fc
md musharruf <mdmusharruf13@gmail.com>
2023-12-20 18:34:26 +0530

    multiple nested elements

--- Chapter_01 Inception/Coding/App.js
+++ Chapter_01 Inception/Coding/App.js
@@ -1,8 +1,26 @@
-const h2 = React.createElement("h2", { className: 'heading' }, "h2 heading");
-const p = React.createElement("p", { className: 'para' }, "Welcome to react learning");
+// const h2 = React.createElement("h2", { className: 'heading' }, "h2 heading");
+// const p = React.createElement("p", { className: 'para' }, "Welcome to react learning");
 
-const container = React.createElement("div", { id: 'container' }, [h2, p]);
+// const container = React.createElement("div", { id: 'container' }, [h2, p]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1> i am h1 tag </h1>
+ *          <h1> i am h1 tag </h1>
+ *      </div>
+ * </div>
+ */
+
+const heading = React.createElement("div",
+  { id: 'parent' },
+  React.createElement("div", { id: 'child' }, [
+    React.createElement("h1", {}, "i'm an h1 tag"),
+    React.createElement("h1", {}, "i'm an h1 tag"),
+  ])
+);
+
+
+root.render(heading);
