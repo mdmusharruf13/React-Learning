d8a39881c5b7d7ac79a5e9c0989802f74bbc915d
md musharruf <mdmusharruf13@gmail.com>
2023-12-21 12:25:33 +0530

    rendering functional component

--- Chapter_03 Laying the Foundation/Coding/App.js
+++ Chapter_03 Laying the Foundation/Coding/App.js
@@ -16,6 +16,15 @@ const heading = React.createElement(
 
 const heading3 = <h3 key="h3" id="title">heading with jsx</h3>
 
+const HeaderComponent = function() {
+    return (
+        <div>
+            <h1>Functional component heading</h1>
+            <h2>This is h2 tag</h2>
+        </div>
+    );
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading3);
+root.render(<HeaderComponent />);
