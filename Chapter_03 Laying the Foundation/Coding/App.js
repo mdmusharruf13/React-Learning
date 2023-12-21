5b55468a1c60d038a650d7b1688be170bd38dab0
md musharruf <mdmusharruf13@gmail.com>
2023-12-21 12:47:03 +0530

    component, variables inside jsx {}

--- Chapter_03 Laying the Foundation/Coding/App.js
+++ Chapter_03 Laying the Foundation/Coding/App.js
@@ -1,43 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// React.createElement => Object => HTML(DOM)
-
-const heading = React.createElement(
-    "h2",
-    {
-        id: "title",
-        key: "h2"
-    },
-    "Heading with h2"
-);
-
 // JSX => React.createElement => Object => HTML(DOM)
 
-const heading3 = <h3 key="h3" id="title">heading with jsx</h3>
+const heading3 = <h3 key="h3" id="title">heading with jsx</h3>;
+const Para = () => <p key="para">this is paragraph</p>;
 
 const HeaderComponent = function () {
     return (
         <div>
             <h1>Functional component heading</h1>
             <h2>This is h2 tag</h2>
+            {heading3}
+            {Para()}
+            <Para />
         </div>
     );
 };
 
-const HeaderComponent2 = () => {
-    return (
-        <div>
-            <h1>Second Functional component heading</h1>
-            <h2>This is h2 tag</h2>
-        </div>
-    )
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <>
-        <HeaderComponent />
-        <HeaderComponent2 />
-    </>
-);
+root.render(<HeaderComponent />);
