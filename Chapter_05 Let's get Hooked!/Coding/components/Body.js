cf73d28f5ddd6afd2cb96b28006b3f1eb2db7750
md musharruf <mdmusharruf13@gmail.com>
2023-12-23 01:06:18 +0530

    search bar implementation

--- Chapter_05 Let's get Hooked!/Coding/components/Body.js
+++ Chapter_05 Let's get Hooked!/Coding/components/Body.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import restaurantList from "../../Constant";
 import ResturantCard from "./ResturantCard";
 
 const Body = () => {
+
+    function filterData(search) {
+        return restaurantList.filter(item => item.data.name.includes(search));
+    }
+    const [inputSearch, setInputSearch] = useState("");
+    const [restaurantItem, setRestaurantItem] = useState(restaurantList);
+
     return (
+        <>
+        <div>
+            <input 
+                type="text" 
+                className="search-box" 
+                placeholder="Search"  
+                onChange={(e) => setInputSearch(e.target.value)} 
+                value={inputSearch}
+            />
+            <button onClick={() => setRestaurantItem(filterData(inputSearch))}>Search</button>
+        </div>
         <div className="restaurant-list">
-            {restaurantList.map((item) => {
+            {restaurantItem.map((item) => {
                 return (<ResturantCard key={item.data.id} {...item.data}/>)
             })}
         </div>
+        </>
     )
 };
 
